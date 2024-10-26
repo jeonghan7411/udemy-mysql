70a1b5fd59d2c32f16b6091fa448b9e620d52c3d
bangjeonghan <aufkrh@naver.com>
2024-10-26 22:54:25 +0900

    142,143 GROUP BY 로 MIN MAX 사용하기

--- 9강-집계함수/142.sql
+++ 9강-집계함수/142.sql
@@ -1,10 +1,22 @@
-Group By 로 Min 및 Max 사용하기
+-- GROUP By 로 Min 및 Max 사용하기
 
-
-SELECT
-	CONCAT(author_fname, ' ', author_lname) as author
-	,	COUNT(*)
-FROM
-	books 
-WHERE 1=1
-GROUP BY author;
+SELECT author_lname, MIN(released_year) FROM books GROUP BY author_lname;
+ 
+SELECT author_lname, MAX(released_year), MIN(released_year) FROM books GROUP BY author_lname;
+ 
+SELECT 
+	author_lname, 
+	COUNT(*) as books_written, 
+	MAX(released_year) AS latest_release,
+	MIN(released_year)  AS earliest_release,
+      MAX(pages) AS longest_page_count
+FROM books GROUP BY author_lname;
+ 
+ 
+SELECT 
+	author_lname, 
+        author_fname,
+	COUNT(*) as books_written, 
+	MAX(released_year) AS latest_release,
+	MIN(released_year)  AS earliest_release
+FROM books GROUP BY author_lname, author_fname;
