304f0c76caa3da1649a7aa868cd0cb5174e3875e
bangjeonghan <aufkrh@naver.com>
2025-01-14 22:54:18 +0900

    크로스 조인

---
+++ 13강-일대다/225. 크로스 조인.sql
@@ -0,0 +1,10 @@
+SELECT id FROM customers WHERE last_name = 'George';
+SELECT * FROM orders WHERE customer_id = 1;
+ 
+ 
+SELECT * FROM orders 
+WHERE customer_id = (SELECT id FROM customers WHERE last_name = 'George');
+ 
+-- To perform a (kind of useless) cross join:
+SELECT * FROM customers, orders;
+-- 모든것을 조합해서 보여준다
